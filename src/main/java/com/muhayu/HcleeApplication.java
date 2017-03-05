4cd9ff682dc39b52532d9c49aeab3784019cb3bb
hclee <hclee@muhaycu.com>
2017-03-06 01:25:25 +0900

    initial commit

---
+++ src/main/java/com/muhayu/HcleeApplication.java
@@ -0,0 +1,11 @@
+package com.muhayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcleeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HcleeApplication.class, args);
+    }
+}

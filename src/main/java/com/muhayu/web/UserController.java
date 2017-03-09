a8fe8769afae1771b14bdd15886f9d7ab1b385d5
hyecheon <hyecheon@hyecheonui-MacBook-Pro.local>
2017-03-09 23:19:25 +0900

    회원가입 , 사용자 목록 기능 구현

--- src/main/java/com/muhayu/web/UserController.java
+++ src/main/java/com/muhayu/web/UserController.java
@@ -1,18 +1,30 @@
 package com.muhayu.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hyecheon on 2017. 3. 7..
  */
 @Controller
 public class UserController {
+    final private List<User> users = new ArrayList<>();
 
     @PostMapping("/create")
     public String create(User user) {
         System.out.println(user.toString());
-        return "index";
+        users.add(user);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
     }
 }

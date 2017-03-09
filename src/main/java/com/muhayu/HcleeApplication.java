cfae6555f5d401552125fa97214e36e350dfee59
hyecheon <hyecheon@hyecheonui-MacBook-Pro.local>
2017-03-10 01:37:16 +0900

    mustache

--- src/main/java/com/muhayu/HcleeApplication.java
+++ src/main/java/com/muhayu/HcleeApplication.java
@@ -1,6 +1,7 @@
 package com.muhayu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/muhayu/web/WelcomeController.java
+++ src/main/java/com/muhayu/web/WelcomeController.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.annotation.HandlesTypes;
+
 /**
  * Created by hyecheon on 2017. 3. 7..
  */
+
 @Controller
 public class WelcomeController {
     @GetMapping("/helloworld")
-    public String welcome(String name, int age, Model model) {
-        System.out.println(name + "name");
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
+    public String welcome(Model model) {
+        model.addAttribute("name", "javajigi");
+        model.addAttribute("value", 10000);
+        model.addAttribute("taxed_value", 30);
+        model.addAttribute("in_ca", true);
         return "welcome";
     }
 }
